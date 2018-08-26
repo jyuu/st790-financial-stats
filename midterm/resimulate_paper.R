747097984f43006d74a18c6c2ed7b80294d598ce
jyuu <jyu21@ncsu.edu>
2018-08-26 18:02:44 -0400

    outline from paper

---
+++ midterm/resimulate_paper.R
@@ -0,0 +1,66 @@
+
+
+# see how she chose papers ------------------------------------------------
+# how many citations? 
+# ask RM... how he goes about looking.. what is his workflow? ? 
+
+
+
+# read  -------------------------------------------------------------------
+
+# 7 “Momentum in Futures Market”, Norges Bank Investment Management, Discussion Note #1-2014.
+
+# get sp500 ---------------------------------------------------------------
+# Portfolio rebalancing can exacerbate drawdowns during major trends, as in 2008-2009 and 2000-2002. 
+
+# get treasury ------------------------------------------------------------
+
+
+# show allocation in port weight ------------------------------------------
+# fig 7 
+# 600 bps at the bottom (Figures 1 and 2)
+# Rebalancing, however, is here to stay. Passive portfolio construction in which weights are allowed to drift is not a realistic alternative, because allocations can get extreme after only a few year
+
+
+# momentum overlay  -------------------------------------------------------
+# operating on daily time scale 
+# But, as divergences in asset performance stabilise, the momentum overlay will cut out and allow the full rebalance to occur.
+# improve the timing of the rebalance.. 
+# FINAL -- use SURVIVAL MODEL to improve the TIMING?? 
+
+
+# autocorrelation ---------------------------------------------------------
+
+
+# fixed weight rebalancing  -----------------------------------------------
+
+
+# drawdowns ---------------------------------------------------------------
+
+# trending environment ----------------------------------------------------
+# equities keep losing relative to bonds 
+
+# trendless environment ---------------------------------------------------
+# characterized by mean reversion 
+# show how rebalancing is good 
+
+
+# momentum overlay  -------------------------------------------------------
+# show how it can hedge the drawdown risks  
+# try 10%, 20% other rates of overlay 
+# the MO is applied as individual trend following strategies  
+# on individual assets with risk allocation set to 10% of the constant mix port 
+
+
+# Mo ----------------------------------------------------------------------
+# moving average cross over model employed at AHL 
+# target volatility of 10%, turns over positions 4 times a yr 
+# show how it increases the sharpe ratio 
+# improves skewness 
+
+
+# take talking pts from movie  --------------------------------------------
+# about how people will make a run on the banks without confidence 
+# the whole system operates on trust 
+# why happens when that is true 
+# 
