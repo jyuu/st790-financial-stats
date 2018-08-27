c5ae8a22ba7c24823c55d40f1864667bbd8ad4fe
jyuu <jyu21@ncsu.edu>
2018-08-26 20:02:49 -0400

    need to write a function for fixing the weights

--- midterm/resimulate_paper.R
+++ midterm/resimulate_paper.R
@@ -1,22 +1,82 @@
+rm(list=ls())
 
+# load --------------------------------------------------------------------
+library(quantmod)
 
 # see how she chose papers ------------------------------------------------
 # how many citations? 
 # ask RM... how he goes about looking.. what is his workflow? ? 
 
-
-
 # read  -------------------------------------------------------------------
-
 # 7 “Momentum in Futures Market”, Norges Bank Investment Management, Discussion Note #1-2014.
 
 # get sp500 ---------------------------------------------------------------
+
+# get the financial data  -------------------------------------------------
 # Portfolio rebalancing can exacerbate drawdowns during major trends, as in 2008-2009 and 2000-2002. 
+# start <- as.Date("2008-01-01")
+# end <- as.Date("2009-12-31")
+# getSymbols("SNP", src = "yahoo", from = start, to = end)
+# getSymbols("^TNX", src = "yahoo", from = start, to = end)
+# saveRDS(SNP, "data/SNP_0809.rds")
+# saveRDS(TNX, "data/TNX_0809.rds")
+# # try different period as well 
+# start <- as.Date("2000-01-01")
+# end <- as.Date("2002-12-31")
+# getSymbols("SNP", src = "yahoo", from = start, to = end)
+# getSymbols("^TNX", src = "yahoo", from = start, to = end)
+# saveRDS(SNP, "data/SNP_0002.rds")
+# saveRDS(TNX, "data/TNX_0002.rds")
 
-# get treasury ------------------------------------------------------------
-
+# viz raw data ------------------------------------------------------------
+TNX1 <- readRDS("data/TNX_0002.rds")
+TNX2 <- readRDS("data/TNX_0809.rds")
+tnx1 <- TNX1$TNX.Open # just the value at the open 
+tnx2 <- TNX2$TNX.Open 
+plot(as.numeric(tnx1), type = "l", ylim = c(0, 7))
+lines(as.numeric(tnx2)) # obviously very different 
+#candleChart(TNX1, up.col = "black", dn.col = "red", theme = "white")
+SNP1 <- readRDS("data/SNP_0002.rds")
+snp1 <- SNP1$SNP.Open
 
 # show allocation in port weight ------------------------------------------
+w1 <- .6 # for stocks 
+w2 <- .4 # for bonds
+# total shares 
+t <- 100 
+# on the day .. 
+numS <- t*w1 
+numT <- t*w2
+snp <- as.numeric(snp1)
+tnx <- as.numeric(tnx1)
+ira <- numS*snp[1] + numT*tnx[1] # initial value 
+# calculate the value over time with this allocation 
+n <- min(length(snp), length(tnx)) # not sure if times line up!
+ira <- numS*snp[1:n] + numT*tnx[1:n] 
+plot(ira, type = "l") 
+# what if we rebalance every month?  
+idx <- seq(1, n, by = 29)
+# example at first month after 
+new_ira <- numS*snp[30] + numT*tnx[30]
+cat("SNP should be: ", w1*new_ira, " but it is at ", numS*snp[30], "\n") 
+cat("TNX should be: ", w2*new_ira, " but it is at ", numT*tnx[30], "\n")
+# let's sell the winners and buy the losers 
+(numS*snp[30] - w1*new_ira) / tnx[30 ]  # what happen when add 24 shares of TNX? 
+(numT*tnx[30] + tnx[30]*24)  # awesome close to w2*new_ira 
+# let's make a function - make sure it applies to EACH time period 
+rebalance <-  function(new, index){
+  if(w1*new - numS*snp[index] < 0){
+    # we want to sell the winner 
+    new_tnx <- (numS*snp[index] - w1*new) / tnx[index]
+  } else {
+    
+  }
+}
+lines()
+# assume monthly is every 30 days 
+idx <- seq(1, length(tnx1), by = 30) 
+plot(idx, tnx1[idx]) 
+
 # fig 7 
 # 600 bps at the bottom (Figures 1 and 2)
 # Rebalancing, however, is here to stay. Passive portfolio construction in which weights are allowed to drift is not a realistic alternative, because allocations can get extreme after only a few year
