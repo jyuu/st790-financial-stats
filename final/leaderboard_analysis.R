bfa45f887ade4b05d5561ce1ff218df19970c481
jyuu <jyu21@ncsu.edu>
2018-11-10 14:39:44 -0500

    final so far

---
+++ final/leaderboard_analysis.R
@@ -0,0 +1,36 @@
+
+# laod --------------------------------------------------------------------
+library(dplyr)
+library(readr)
+library(xtable)
+
+# read --------------------------------------------------------------------
+#dat <- read_csv("final/daily-leaderboard-2018-11-02.csv") 
+dat <- read_csv("final/daily-leaderboard-2018-11-08.csv") 
+
+dat_filtered <- dat %>%
+  select(rank, 
+         name, 
+         score, 
+         max_beta_to_spy_126day, 
+         max_cumulative_common_returns, 
+         max_leverage, 
+         max_max_drawdown,
+         max_net_dollar_exposure,
+         max_total_returns,
+         min_total_returns,
+         max_turnover, 
+         max_volatility_126day) 
+
+all <- dat_filtered %>%
+  summarise_all(funs(mean)) %>% 
+  mutate_at(vars(-name), funs(round(., 3))) %>%
+  tidyr::gather(Metric, Result)
+
+joyce <- dat_filtered %>% 
+  filter(rank == 64)  %>% 
+  mutate_at(vars(-name), funs(round(., 3))) %>%
+  tidyr::gather(Metric, Result) %>%
+  bind_cols(all[,2])
+
+xtable(joyce)
